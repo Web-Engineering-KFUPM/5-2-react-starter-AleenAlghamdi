8666b47112e64de58e35bc09c6fd25de3a7f81ff
AleenAlghamdi <s202244260@kfupm.edu.sa>
2025-10-01 15:02:23 +0300

    Task 1: Create a StudentCard Component

---
+++ 5-2-react-starter/src/components/StudentCard.jsx
@@ -0,0 +1,11 @@
+function StudentCard() {
+    return (
+        <div>
+            <h3>Name: Aleen Alghamdi</h3>
+            <p>ID: 202244260</p>
+            <p>Department: Information & Computer Science</p>
+        </div>
+    );
+}
+
+export default StudentCard;
